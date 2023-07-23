29a3c96b0a3734587d5fd756fabf320afc50cde1
Rahul Ghimire <ghimirerahul554@gmail.com>
2023-07-23 09:09:00 +0545

    Add navbar in the app

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../index.css'
+const TopBar = () => {
+  return (
+    <div className='topbar d-flex justify-content-center'>
+        <p className='topbar_text'>Call us for Veterinary Support: +977-9861859764</p>
+    </div>
+  )
+}
+
+export default TopBar
